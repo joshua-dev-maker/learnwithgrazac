5a098cea1d5379162252fb7940c42475e5069ba2
darkweb455 <ojojoshua18@gmail.com>
2022-04-13 11:35:21 +0100

    new port and database

--- src/db/mySQLcon.db.js
+++ src/db/mySQLcon.db.js
@@ -1,23 +1,3 @@
-// const mysql = require("mysql2");
-// require("dotenv").config();
-
-// const dataBase = async () => {
-//   try {
-//     const conn = await mysql.createConnection({
-//       host: "localhost",
-//       user: "root",
-//       password: process.env.mysql_password,
-//       dataBase: "learngrazac",
-//     });
-
-//     console.log("MySQL server is running");
-//   } catch (error) {
-//     console.log(error.message);
-//   }
-// };
-
-// module.exports = dataBase;
-
 const mysql = require("mysql2");
 const dotenv = require("dotenv");
 dotenv.config();
@@ -26,7 +6,7 @@ const database = mysql.createConnection({
   host: process.env.mysql_host,
   user: process.env.mysql_username,
   password: process.env.mysql_password,
-  database: "learngrazac",
+  database: "database11",
   port: process.env.mysql_port,
 });
 
